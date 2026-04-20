d3b12e58d5d262552b4d81a22e4b809004b30958
abdallah-ehab <abdallahehab3710@gmail.com>
2026-04-20 12:28:39 +0200

    Initial commit

--- src/pages/teacher/SupportPage.tsx
+++
@@ -1,7 +0,0 @@
-export function SupportPage() {
-  return (
-    <div className="flex items-center justify-center h-full p-md">
-      <p className="font-body-md text-body-md text-on-surface-variant">Support page coming soon.</p>
-    </div>
-  )
-}
