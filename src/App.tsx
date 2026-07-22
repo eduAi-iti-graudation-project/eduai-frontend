690d8f9ad7a60dd9989f2bd422a95fea45b67c4b
abdallah-ehab <abdallahehab3710@gmail.com>
2026-07-22 11:32:34 +0300

    feat: switch to RouterProvider and remove BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,11 @@
-import { Routes, Route, Navigate } from "react-router-dom"
+import { RouterProvider } from "react-router-dom"
 import { Toaster } from "sonner"
-import { LoginPage } from "./pages/LoginPage"
-import { SignupPage } from "./pages/SignupPage"
-import { TeacherDashboardPage } from "./pages/TeacherDashboardPage"
-import { StudentPortalPage } from "./pages/StudentPortalPage"
-import { ClassesPage } from "./pages/teacher/ClassesPage"
-import { ClassDetailPage } from "./pages/teacher/ClassDetailPage"
-import { InstructorAssignmentForm } from "./components/InstructorAssignmentForm"
-import { RubricsPage } from "./pages/teacher/RubricsPage"
-import { SubmissionsPage } from "./pages/teacher/SubmissionsPage"
-import { AlertsPage } from "./pages/teacher/AlertsPage"
-import { AssistantPage } from "./pages/teacher/AssistantPage"
-import { SettingsPage } from "./pages/teacher/SettingsPage"
-import { SupportPage } from "./pages/teacher/SupportPage"
-import { NotificationsListPage } from "./pages/teacher/NotificationsListPage"
-import { TeacherLayout } from "./components/layout/TeacherLayout"
-import { TeacherRoute, StudentRoute, RootRedirect } from "./components/auth/RouteGuards"
+import { router } from "./router"
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<RootRedirect />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-
-        {/* Teacher Portal */}
-        <Route path="/dashboard" element={<TeacherRoute><TeacherDashboardPage /></TeacherRoute>} />
-        <Route path="/classes" element={<TeacherRoute><ClassesPage /></TeacherRoute>} />
-        <Route path="/classes/:id" element={<TeacherRoute><ClassDetailPage /></TeacherRoute>} />
-        <Route path="/assignments/new" element={<TeacherRoute><InstructorAssignmentForm /></TeacherRoute>} />
-        <Route path="/rubrics" element={<TeacherRoute><RubricsPage /></TeacherRoute>} />
-        <Route path="/rubrics/new" element={<TeacherRoute><RubricsPage /></TeacherRoute>} />
-        <Route path="/submissions" element={<TeacherRoute><SubmissionsPage /></TeacherRoute>} />
-        <Route path="/submissions/:id" element={<TeacherRoute><SubmissionsPage /></TeacherRoute>} />
-        <Route path="/alerts" element={<TeacherRoute><AlertsPage /></TeacherRoute>} />
-        <Route path="/assistant" element={<TeacherRoute><AssistantPage /></TeacherRoute>} />
-
-        {/* Teacher Layout Routes */}
-        <Route element={<TeacherRoute><TeacherLayout /></TeacherRoute>}>
-          <Route path="/notifications" element={<NotificationsListPage />} />
-        </Route>
-        <Route path="/settings" element={<TeacherRoute><SettingsPage /></TeacherRoute>} />
-        <Route path="/support" element={<TeacherRoute><SupportPage /></TeacherRoute>} />
-
-        {/* Student Portal */}
-        <Route path="/student-portal" element={<StudentRoute><StudentPortalPage /></StudentRoute>} />
-        <Route path="/student-portal/*" element={<StudentRoute><StudentPortalPage /></StudentRoute>} />
-
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <RouterProvider router={router} />
       <Toaster richColors position="top-right" />
     </>
   )
